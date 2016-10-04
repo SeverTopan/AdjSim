182521873693e593d0d4fa48df1c1df429c614b8
= <severtopan@yahoo.ca>
2016-10-04 21:15:37 +0200

    Implemented groundwork for target finding and ability casting

--- AdjEcosystem.py
+++ AdjEcosystem.py
@@ -5,16 +5,194 @@
 # * Enivronment is set up via a configuration script at the beginning of each
 # * simulation that sets cycle schedules, resource locations, etc.
 #
-# By Sever Topan
+# Deisgned and developed by Sever Topan
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# IMPORTS
 #-------------------------------------------------------------------------------
 import sys
+import re
+import inspect
+import random
 
-def horizontalRuler(length):
-    msg = ""
-    for i in range(length):
-        msg += "-"
+#-------------------------------------------------------------------------------
+# CLASS RESOURCE
+#-------------------------------------------------------------------------------
+class Resource():
+    """docstring for Resource."""
 
-    return msg
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self, environment, name, blockedDuration = 0):
+        self.environment = environment
+        self.name = name
+        self.blockedDuration = blockedDuration
+
+#-------------------------------------------------------------------------------
+# CLASS AGENT
+#-------------------------------------------------------------------------------
+class Agent(Resource):
+    """docstring for Agent."""
+
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self, abilities = {}, traits = {}):
+        super(Agent, self).__init__()
+        self.ability = abilities
+        self.trait = traits
+
+#-------------------------------------------------------------------------------
+# CLASS ABILITY
+#-------------------------------------------------------------------------------
+class Ability(Resource):
+    """docstring for Ability."""
+
+    targets = {}
+
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self, agent, targets, duration):
+        super(Ability, self).__init__()
+        self.agent = agent
+        self.targets = targets
+        self.duration = duration
+
+# METHOD CAST
+#-------------------------------------------------------------------------------
+    def cast(self):
+        # clear and obtain targets
+        for target in self.targets:
+            target.clear()
+            target.obtain()
+
+            if target.resource == None:
+                return False
+
+        # perform target effects and blocks
+        for target in targets:
+            target.performEffects
+            target.performBlock
+
+        return True
+
+
+#-------------------------------------------------------------------------------
+# CLASS TARGET
+#-------------------------------------------------------------------------------
+class Target():
+    """docstring for Target."""
+
+    conditions = []
+    effects = []
+    blockedDuration = 0;
+    resource = None
+
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self, environment, conditions, effects, blockedDuration):
+        self.environment = environment
+        self.conditions = conditions
+        self.effects = effects
+        self.blockedDuration = blockedDuration
+
+# METHOD CLEAR
+#-------------------------------------------------------------------------------
+    def clear(self):
+        self.resource = None
+
+# METHOD MATCHED
+#-------------------------------------------------------------------------------
+    def matched(self):
+        return (self.resource != None)
+
+# METHOD OBTAIN
+#-------------------------------------------------------------------------------
+    def obtain(self):
+        potentialTargets = {}
+
+        # on no conditions, obtain random resource from environment
+        if len(conditions) == 0:
+            resource = environment.getTraitRandom()
+        else:
+            potentialTargets = potentialTargets \
+                | environment.getTraitsOnCondition(condition[0])
+
+        # intersect sets to narrow
+        for condition in conditions:
+            potentialTargets = potentialTargets \
+                & environment.getTraitsOnCondition(condition)
+            if len(potentialTargets) == 0:
+                resource = None;
+                return false;
+
+        # for now, simply choose the first out of the list of targets
+        resource = potentialTargets[0]
+
+        return True
+
+# METHOD PERFORM EFFECTS
+#-------------------------------------------------------------------------------
+    def performEffects(self):
+        for effect in effects:
+            effect(resource)
+
+# METHOD PERFORM BLOCK
+#-------------------------------------------------------------------------------
+    def performBlock(self):
+        resource.blockedDuration = blockedDuration
+
+
+#-------------------------------------------------------------------------------
+# CLASS TRAIT
+#-------------------------------------------------------------------------------
+class Trait(Resource):
+    """docstring for Trait."""
+
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self, agent, name, value):
+        super(Trait, self).__init__()
+        self.agent = agent
+        self.name = name
+        self.value = value
+        self.isBlocked = isBlocked
+
+
+
+#-------------------------------------------------------------------------------
+# CLASS ENVIRONMENT
+#-------------------------------------------------------------------------------
+class Environment():
+    """docstring for Environment."""
+
+    agentList = {}
+    time = None
+
+# METHOD __INIT__
+#-------------------------------------------------------------------------------
+    def __init__(self):
+        self.time = 0
+
+# METHOD GET TRAIT RANDOM
+#-------------------------------------------------------------------------------
+    def getTraitRandom(self):
+        selectedAgent = random.random() * len(self.agentList)
+        selectedTrait = random.random() * len(selectedAgent.traits)
+
+        return selectedAgent.traits[selectedTrait]
+
+# METHOD GET TRAIT ON CONDITION
+#-------------------------------------------------------------------------------
+    def getTraitsOnCondition(self, condition):
+        traitSet = {}
+
+        for agent in self.agentList:
+            for trait in agent.traits:
+                if condition(trait):
+                    traitSet.add(trait)
+
+        return traitSet
 
 #-------------------------------------------------------------------------------
 # CLASS ADJECOSYSTEM
@@ -22,11 +200,25 @@ def horizontalRuler(length):
 class AdjEcosystem:
     """docstring for AdjEcosystem."""
 
+    environment = None
+
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
     def __init__(self, argv):
+        self.environment = Environment()
         self.printWelcome()
-        self.parseConfigFile(argv)
+
+        self.generateTestClasses()
+
+# METHOD HORIZONTAL RULER
+#-------------------------------------------------------------------------------
+    @staticmethod
+    def horizontalRuler(length):
+        msg = ""
+        for i in range(length):
+            msg += "-"
+
+        return msg
 
 # METHOD PRINT WELCOME
 #-------------------------------------------------------------------------------
@@ -34,9 +226,9 @@ class AdjEcosystem:
     def printWelcome():
         welcomeMessage = "- AdjEcosystem -"
 
-        print horizontalRuler(len(welcomeMessage))
+        print AdjEcosystem.horizontalRuler(len(welcomeMessage))
         print welcomeMessage
-        print horizontalRuler(len(welcomeMessage))
+        print AdjEcosystemselfself.horizontalRuler(len(welcomeMessage))
 
 # METHOD PARSE CONFIG FILE
 #-------------------------------------------------------------------------------
@@ -53,6 +245,18 @@ class AdjEcosystem:
 
         print "Parsing Config file..."
 
+
+
+        configData = configFile.read()
+        agentMatches = re.findall('(agent:\\n(.+\\n)+)+?', configData)
+
+        # unfinished, will add config file parsing functionality after core
+        # functionality is ompletely implemented
+
+        return
+
+    def generateTestClasses(self):
+        return
 
 
 #-------------------------------------------------------------------------------
