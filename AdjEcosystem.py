6f19db1a73156e187bdfa233a0cd0465b757d4d9
= <severtopan@yahoo.ca>
2016-10-10 17:54:12 +0200

    Implemented ability target acquisition & casting
    Also added test class generation within AdjEcosystem class for debugging

--- AdjEcosystem.py
+++ AdjEcosystem.py
@@ -19,15 +19,15 @@ import random
 #-------------------------------------------------------------------------------
 # CLASS RESOURCE
 #-------------------------------------------------------------------------------
-class Resource():
+class Resource(object):
     """docstring for Resource."""
 
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
-    def __init__(self, environment, name, blockedDuration = 0):
+    def __init__(self, environment, name):
         self.environment = environment
         self.name = name
-        self.blockedDuration = blockedDuration
+        blockedDuration = 0
 
 #-------------------------------------------------------------------------------
 # CLASS AGENT
@@ -37,10 +37,24 @@ class Agent(Resource):
 
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
-    def __init__(self, abilities = {}, traits = {}):
-        super(Agent, self).__init__()
-        self.ability = abilities
-        self.trait = traits
+    def __init__(self, environment, name, x = 0, y = 0):
+        super(Agent, self).__init__(environment, name)
+        self.abilities = {}
+        self.traits = {}
+        self.addMandatoryTraits(x, y)
+
+# METHOD ADD TRAIT
+#-------------------------------------------------------------------------------
+    def addTrait(self, name, value):
+        self.traits[name] = Trait(self.environment, name, value)
+
+
+# METHOD ADD MANDATORY TRAITS
+#-------------------------------------------------------------------------------
+    def addMandatoryTraits(self, x, y):
+        self.addTrait('exists', True)
+        self.addTrait('xCoord', x)
+        self.addTrait('yCoord', y)
 
 #-------------------------------------------------------------------------------
 # CLASS ABILITY
@@ -48,99 +62,133 @@ class Agent(Resource):
 class Ability(Resource):
     """docstring for Ability."""
 
-    targets = {}
-
 # METHOD __INIT__
+# * pre-parsed version
 #-------------------------------------------------------------------------------
-    def __init__(self, agent, targets, duration):
-        super(Ability, self).__init__()
+    def __init__(self, environment, name, agent, predicates, condition, effects, blockers):
+        super(Ability, self).__init__(environment, name)
         self.agent = agent
-        self.targets = targets
-        self.duration = duration
+        self.predicates = predicates
+        self.condition = condition
+        self.effects = effects
+        self.blockers = blockers
+
+# METHOD CHECK TARGET SET COMBINATIONS
+# * recursively checks all combinations of potential targets and yields valid
+# * target combination sets. Function is recursive, since its complexity is
+# * O(n^k), where k is the number of potential targets
+#-------------------------------------------------------------------------------
+    def checkTargetSetCombinations(self, potentialTargetSet, validTargetSet, targetIndex = [0]):
+        currIndex = len(targetIndex) - 1
+
+        # error check
+        if len(potentialTargetSet) == 0:
+            raise Exception('Invalid potentialTargetSet')
+
+        for currTarget in potentialTargetSet[currIndex]:
+            targetIndex[currIndex] = currTarget
+            # if at end of recursion expansion, check condition, if not, keep
+            # recursing until end is reached
+            if currIndex + 1 == len(potentialTargetSet):
+                print "   At end of recursion expansion:"
+                for target in targetIndex:
+                    print "     ", target.name
+
+                if self.condition(targetIndex):
+                    print '   yielding: ', targetIndex
+                    validTargetSet.append(targetIndex)
+            else:
+                # init next targetIndex Entry
+                if len(targetIndex) == currIndex + 1:
+                    targetIndex.append(0)
+                # recurse
+                return self.checkTargetSetCombinations(potentialTargetSet, validTargetSet, targetIndex)
+
+        # exit condition
+        if currIndex + 1 == len(potentialTargetSet):
+            return
+
+# METHOD GET POTENTIAL TARGET SET
+# * returns a list of sets of valid targets on which to perfrom actions
+#-------------------------------------------------------------------------------
+    def getPotentialTargets(self):
+        potentialTargetSet = []
+        validTargetSet = []
+
+        for target, predicate in self.predicates:
+            if target == 0:
+                # target = self - check predicate
+                if predicate(self.agent):
+                    # add list entry if not done before
+                    if not potentialTargetSet:
+                        potentialTargetSet.append({self.agent})
+                else:
+                    return None
+
+            else:
+                newPotentialAgents = self.environment.getAgentsOnPredicate(predicate)
+
+                # exit if no targets
+                if not newPotentialAgents:
+                    return None
+
+                # insert new potential agents into potentialTargetSet
+                # !!! tuple element 0 of predictes must be in sorted order
+                if len(potentialTargetSet) == target:
+                    potentialTargetSet.append(newPotentialAgents)
+                else:
+                    potentialTargetSet[target] = \
+                        potentialTargetSet[target] & newPotentialAgents
+
+                # check if intersection yields null set
+                if not potentialTargetSet[target]:
+                    return None
+
+        print "   Potential Target Set: "
+        for agentSet in potentialTargetSet:
+            print "   ---"
+            for agent in agentSet:
+                print "     ", agent.name
+
+
+
+        # accumulate target sets for decision
+        self.checkTargetSetCombinations(potentialTargetSet, validTargetSet)
+
+        if not validTargetSet:
+            return None
+        else:
+            return validTargetSet
+
+# METHOD CHOOSE TARGET SET
+# * performs decision making to determine which target set to perform an ability on
+#-------------------------------------------------------------------------------
+    def chooseTargetSet(self, validTargetSet):
+        # error check
+        if not validTargetSet:
+            raise Exception('Empty validTargetSet in method chooseTargetSet')
+
+        # here, insert logic for higher level decision making
+        # for now, return first set in the list
+        return validTargetSet[0]
 
 # METHOD CAST
 #-------------------------------------------------------------------------------
-    def cast(self):
-        # clear and obtain targets
-        for target in self.targets:
-            target.clear()
-            target.obtain()
+    def cast(self, targets):
 
-            if target.resource == None:
-                return False
+        # error check
+        if not targets:
+            raise Exception('No targets in method cast')
 
-        # perform target effects and blocks
-        for target in targets:
-            target.performEffects
-            target.performBlock
+        # perform target effects
+        for effect in self.effects:
+            effect(targets)
 
-        return True
-
-
-#-------------------------------------------------------------------------------
-# CLASS TARGET
-#-------------------------------------------------------------------------------
-class Target():
-    """docstring for Target."""
-
-    conditions = []
-    effects = []
-    blockedDuration = 0;
-    resource = None
-
-# METHOD __INIT__
-#-------------------------------------------------------------------------------
-    def __init__(self, environment, conditions, effects, blockedDuration):
-        self.environment = environment
-        self.conditions = conditions
-        self.effects = effects
-        self.blockedDuration = blockedDuration
-
-# METHOD CLEAR
-#-------------------------------------------------------------------------------
-    def clear(self):
-        self.resource = None
-
-# METHOD MATCHED
-#-------------------------------------------------------------------------------
-    def matched(self):
-        return (self.resource != None)
-
-# METHOD OBTAIN
-#-------------------------------------------------------------------------------
-    def obtain(self):
-        potentialTargets = {}
-
-        # on no conditions, obtain random resource from environment
-        if len(conditions) == 0:
-            resource = environment.getTraitRandom()
-        else:
-            potentialTargets = potentialTargets \
-                | environment.getTraitsOnCondition(condition[0])
-
-        # intersect sets to narrow
-        for condition in conditions:
-            potentialTargets = potentialTargets \
-                & environment.getTraitsOnCondition(condition)
-            if len(potentialTargets) == 0:
-                resource = None;
-                return false;
-
-        # for now, simply choose the first out of the list of targets
-        resource = potentialTargets[0]
+        # perform target blocks
+        for blocker in self.blockers:
+            blocker(targets)
 
         return True
-
-# METHOD PERFORM EFFECTS
-#-------------------------------------------------------------------------------
-    def performEffects(self):
-        for effect in effects:
-            effect(resource)
-
-# METHOD PERFORM BLOCK
-#-------------------------------------------------------------------------------
-    def performBlock(self):
-        resource.blockedDuration = blockedDuration
 
 
 #-------------------------------------------------------------------------------
@@ -151,27 +199,20 @@ class Trait(Resource):
 
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
-    def __init__(self, agent, name, value):
-        super(Trait, self).__init__()
-        self.agent = agent
-        self.name = name
+    def __init__(self, environment, name, value):
+        super(Trait, self).__init__(environment, name)
         self.value = value
-        self.isBlocked = isBlocked
-
-
 
 #-------------------------------------------------------------------------------
 # CLASS ENVIRONMENT
 #-------------------------------------------------------------------------------
-class Environment():
+class Environment(object):
     """docstring for Environment."""
-
-    agentList = {}
-    time = None
 
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
     def __init__(self):
+        self.agentList = []
         self.time = 0
 
 # METHOD GET TRAIT RANDOM
@@ -182,33 +223,37 @@ class Environment():
 
         return selectedAgent.traits[selectedTrait]
 
-# METHOD GET TRAIT ON CONDITION
+# METHOD GET AGENT BY NAME
 #-------------------------------------------------------------------------------
-    def getTraitsOnCondition(self, condition):
-        traitSet = {}
+    def getAgentByName(self, name):
+        for agent in self.agentList:
+            if agent.name == name:
+                return agent
+
+        return None
+
+# METHOD GET TRAIT ON PREDICATE
+#-------------------------------------------------------------------------------
+    def getAgentsOnPredicate(self, predicate):
+        agentSet = set()
 
         for agent in self.agentList:
-            for trait in agent.traits:
-                if condition(trait):
-                    traitSet.add(trait)
+            if predicate(agent):
+                agentSet.add(agent)
 
-        return traitSet
+        return agentSet
 
 #-------------------------------------------------------------------------------
 # CLASS ADJECOSYSTEM
 #-------------------------------------------------------------------------------
-class AdjEcosystem:
+class AdjEcosystem(object):
     """docstring for AdjEcosystem."""
-
-    environment = None
 
 # METHOD __INIT__
 #-------------------------------------------------------------------------------
     def __init__(self, argv):
         self.environment = Environment()
-        self.printWelcome()
-
-        self.generateTestClasses()
+        AdjEcosystem.printWelcome()
 
 # METHOD HORIZONTAL RULER
 #-------------------------------------------------------------------------------
@@ -228,7 +273,7 @@ class AdjEcosystem:
 
         print AdjEcosystem.horizontalRuler(len(welcomeMessage))
         print welcomeMessage
-        print AdjEcosystemselfself.horizontalRuler(len(welcomeMessage))
+        print AdjEcosystem.horizontalRuler(len(welcomeMessage))
 
 # METHOD PARSE CONFIG FILE
 #-------------------------------------------------------------------------------
@@ -251,15 +296,102 @@ class AdjEcosystem:
         agentMatches = re.findall('(agent:\\n(.+\\n)+)+?', configData)
 
         # unfinished, will add config file parsing functionality after core
-        # functionality is ompletely implemented
+        # functionality is completely implemented
 
         return
 
+# METHOD GENERATE TEST CLASSES
+#-------------------------------------------------------------------------------
     def generateTestClasses(self):
+        # create dog agent
+        dog = Agent(self.environment, "dog", 5, 5)
+        dog.addTrait('type', 'animal')
+        dog.addTrait('calories', 500)
+        dog.addTrait('eatRange', 5)
+        self.environment.agentList.append(dog)
+
+        # create apple agent
+        apple = Agent(self.environment, "apple", 3, 3)
+        apple.addTrait('type', 'food')
+        apple.addTrait('calories', 35)
+        self.environment.agentList.append(apple)
+
+        # ability eat condition: food.type == 'food'
+        #       && ((food.x - self.x)^2 + (food.y - self.y)^2)^0.5 < self.eatRange
+        # !!! blocked duration predicates
+        # !!! always sort predicate list before insertion into class
+        def eat_predicate_food_type(food):
+            return food.traits.get('type') != None
+        eat_predicate_food_x = lambda food: food.traits.get('xCoord') != None
+        eat_predicate_food_y = lambda food: food.traits.get('yCoord') != None
+        eat_predicate_food_calories = lambda food: food.traits.get('calories') != None
+        eat_predicate_self_eatRange = lambda s: s.traits.get('eatRange') != None
+        eat_predicate_self_x = lambda s: s.traits.get('xCoord') != None
+        eat_predicate_self_y = lambda s: s.traits.get('yCoord') != None
+        eat_predicate_self_calories = lambda s: s.traits.get('calories') != None
+        eat_predicateList = [(0, eat_predicate_self_eatRange), \
+            (0, eat_predicate_self_x), (0, eat_predicate_self_y), \
+            (0, eat_predicate_self_calories), (1, eat_predicate_food_type), \
+            (1, eat_predicate_food_x), (1, eat_predicate_food_y), \
+            (1, eat_predicate_food_calories)]
+
+        eat_condition = lambda targets: targets[1].traits['type'].value == 'food' \
+            and ((targets[1].traits['xCoord'].value - targets[0].traits['xCoord'].value)**2 \
+            + (targets[1].traits['yCoord'].value - targets[0].traits['yCoord'].value)**2)**0.5 \
+            < targets[0].traits['eatRange'].value
+
+        def eat_effect_addCalories(targets):
+            targets[0].traits['calories'].value += targets[1].traits['calories'].value
+        def eat_effect_killFood(targets):
+            targets[1].traits['exists'].value = False
+        eat_effectList = [eat_effect_killFood, eat_effect_addCalories]
+
+        def eat_blocker_dog(targets):
+            targets[0].blockedDuration = 10
+        eat_blockerList = [eat_blocker_dog]
+
+        ability_eat = Ability(self.environment, "eat", dog, eat_predicateList, \
+            eat_condition, eat_effectList, eat_blockerList)
+
+        dog.abilities["eat"] = ability_eat
+
+        # output debug data
+        print "   Agents:"
+        for i in range(len(self.environment.agentList)):
+            print "   ", i, ": ", self.environment.agentList[i].name
+            for key, val in self.environment.agentList[i].traits.items():
+                print "      ", key, ": ", val.value
+
+
         return
+
+# METHOD EXECUTE TEST
+#-------------------------------------------------------------------------------
+    def executeTest(self):
+        print "Testing dog and apple generation:"
+        self.generateTestClasses()
+        print "...done"
+
+        print "Testing predicate & condition casting:"
+        dog_eat = self.environment.getAgentByName('dog').abilities['eat']
+        potentialTargets = dog_eat.getPotentialTargets()
+        print "Potential targets: ", potentialTargets
+
+        print "Testing effect & blocker casting"
+        chosenTargets = dog_eat.chooseTargetSet(potentialTargets)
+        dog_eat.cast(chosenTargets)
+        print "...done"
+
+        print "Result:"
+        print "   Agents:"
+        for i in range(len(self.environment.agentList)):
+            print "   ", i, ": ", self.environment.agentList[i].name
+            for key, val in self.environment.agentList[i].traits.items():
+                print "      ", key, ": ", val.value
 
 
 #-------------------------------------------------------------------------------
 # MAIN EXECUTION SCRIPT
 #-------------------------------------------------------------------------------
 adjEcosystem = AdjEcosystem(sys.argv)
+adjEcosystem.executeTest()
