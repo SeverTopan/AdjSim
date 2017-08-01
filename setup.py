48ce0f63a597db53559ea7a48d384ca757865fb5
SeverTopan <severtopan@yahoo.ca>
2017-08-01 09:04:14 -0400

    Added PyQt Dependency

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(name='AdjSim',
       author='Sever Topan',
       packages=['AdjSim'],
       license='GPL-3.0',
-      install_requires=['matplotlib']
+      install_requires=['matplotlib', 'pyqt']
       )
