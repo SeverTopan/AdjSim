eb91980bc66eadfbb8955ea3abaf1f8e4e0c98c0
SeverTopan <severtopan@yahoo.ca>
2017-09-22 22:30:25 -0400

    Implement decision module.

---
+++ adjsim/decision.py
@@ -0,0 +1,47 @@
+
+# ADJSIM SIMULATION FRAMEWORK
+# Designed and developed by Sever Topan
+
+# IMPORTS
+# standard
+import os
+import sys
+import re
+import pickle
+import random
+
+from . import utility
+
+class Decision(object):
+
+    def __call__(self, simulation, source):
+        raise NotImplementedError
+
+class RandomSingleCastDecision(Decision):
+
+    def __call__(self, simulation, source):
+        # If no actions to choose from, skip.
+        if len(source.actions) == 0:
+            return
+
+        # Randomly execute an action.
+        try:
+            action = random.choice(list(source.actions.values()))
+            action(simulation, source)
+        except TypeError:
+            raise utility.InvalidActionException
+
+class RandomRepeatedCastDecision(Decision):
+
+    def __call__(self, simulation, source):
+        # If no actions to choose from, skip.
+        if len(source.actions) == 0:
+            return
+
+        # Randomly execute an action while the agent has not completed their timestep.
+        while not source.step_complete:
+            try:
+                action = random.choice(list(source.actions.values()))
+                action(simulation, source)
+            except TypeError:
+                raise utility.InvalidActionException
